142548fe779134df44747b2586e0cd59feddf860
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2017-10-24 20:05:34 +0200

    changed error

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ import { analytics } from './analytics';
 import isValidUACode from './utilities/isValidUACode';
 
 export function register(options = {}) {
-	this.trackingId = '';
 	this.verbose = false;
 
 	Object.assign(this, options);
 
 	if (this.trackingId === undefined || !isValidUACode(this.trackingId)) {
-		console.error('Please provide a valid UA code (UA-xxxx-yy)');
+		console.error('Please provide a valid UA code (UA-XXXXXXXX-YY)');
 		return;
 	}
 
