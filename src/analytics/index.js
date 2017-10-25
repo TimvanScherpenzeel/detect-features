ca2434372b13ae9a6159a2bf254f6080c17f462c
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2017-10-26 00:10:51 +0200

    removed base setup, rethink recursive function

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -4,6 +4,46 @@ import { record } from '../utilities/record';
 // Features
 import { getFeatures } from '../features';
 
+let category;
+let feature;
+let value;
+
+const createEventObject = () => {
+	switch (typeof value) {
+		case 'boolean':
+		case 'number':
+			console.log(category, feature, value);
+			record({
+			 	eventCategory: `${category}`,
+			 	eventAction: `${feature}`,
+			 	eventValue: `${value}`,
+			});
+			break;
+		case 'string':
+			console.log(category, feature, value);
+			record({
+				eventCategory: `${category}`,
+				eventAction: `${feature}`,
+				eventLabel: `${value}`,
+			});
+			break;
+		case 'object':
+			const _value = value;
+			const _feature = feature;
+
+			Object.keys(_value).forEach((data) => {
+				category = category.concat(' ' + _feature);
+				feature = data;
+				value = _value[data];
+				createEventObject();
+			});
+			break;
+		default:
+			console.warn(`Type of value: ${value} could not be recognized`)
+			break;
+	}
+};
+
 export const analytics = {
 	initialize: (trackingId) => {
 		// Inject Google Analytics
@@ -21,26 +61,13 @@ export const analytics = {
 	register: (verbose = false) => {
 		const features = getFeatures(verbose);
 
-		Object.keys(features).forEach((category) => {
-			Object.keys(features[category]).forEach((feature) => {
-				const value = features[category][feature];
+		Object.keys(features).forEach((_category) => {
+			Object.keys(features[_category]).forEach((_feature) => {
+				category = _category;
+				feature = _feature;
+				value = features[category][feature];
 
-				if (typeof value === 'boolean' || typeof value === 'number') {
-					record({
-						eventCategory: `${category}`,
-						eventAction: `${feature}`,
-						eventValue: `${value}`,
-					});
-				} else if (typeof value === 'string') {
-					record({
-						eventCategory: `${category}`,
-						eventAction: `${feature}`,
-						eventLabel: `${value}`,
-					});
-				} else {
-					// WebGLFeatures and WebGL2Features
-					// console.log(value);
-				}
+				createEventObject();
 			});
 		});
 	},
