124b120bf53959090eadaa4a282dd02befe0ad6b
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2017-10-25 20:13:49 +0200

    supportedExtensions array values are now an object for easier analytics events

--- src/features/browserFeatures/getWebGLFeatures.js
+++ src/features/browserFeatures/getWebGLFeatures.js
@@ -15,9 +15,7 @@ export default (() => {
 		shaderVersion: gl.getParameter(gl.SHADING_LANGUAGE_VERSION),
 		rendererUnmasked: glExtensionDebugRendererInfo && gl.getParameter(glExtensionDebugRendererInfo.UNMASKED_RENDERER_WEBGL),
 		vendorUnmasked: glExtensionDebugRendererInfo && gl.getParameter(glExtensionDebugRendererInfo.UNMASKED_VENDOR_WEBGL),
-
-		supportedExtensions: gl.getSupportedExtensions(),
-		antialias: !!gl.getContextAttributes().antialias,
+		supportedExtensions: gl.getSupportedExtensions().reduce((obj, key) => ({ ...obj, [key]: true}), {}),
 	};
 
 	return features;
