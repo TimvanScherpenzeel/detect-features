717abbe5ba1184b4ab623db838fa4d2e559f72eb
Tim van Scherpenzeel <tim.vanscherpenzeel@random.nu>
2018-01-25 12:39:17 +0100

    removed google analytics, will set up my own tracking solution

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 // Analytics
 import { analytics } from './analytics';
 
-// Utilities
-import isValidUACode from './utilities/isValidUACode';
-
 export function register(options = {}) {
 	this.verbose = false;
 	this.log = false;
 
 	Object.assign(this, options);
 
-	if (this.trackingId === undefined || !isValidUACode(this.trackingId)) {
-		console.error('Please provide a valid UA code (UA-XXXXXXXX-YY)');
-		return;
-	}
-
-	analytics.initialize(this.trackingId);
 	analytics.register(this.verbose, this.log);
 }
--- src/utilities/isValidUACode.js
+++
@@ -1,3 +0,0 @@
-export default function isValidUACode(str) {
-    return (/^ua-\d{4,9}-\d{1,4}$/i).test(str.toString());
-}
