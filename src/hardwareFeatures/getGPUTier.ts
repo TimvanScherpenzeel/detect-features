a059e9c474f3092237de31998a3fbbca494edbe0
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-18 16:28:14 +0200

    remove unused file

--- src/hardwareFeatures/getGPUTier.ts
+++
@@ -1,8 +0,0 @@
-// Vendor
-import { getGPUTier } from 'detect-gpu';
-
-export default ((): { tier: string; type: string } =>
-  getGPUTier({
-    desktopBenchmarkPercentages: [0, 50, 30, 20], // (Default) [TIER_0, TIER_1, TIER_2, TIER_3]
-    mobileBenchmarkPercentages: [0, 50, 30, 20], // (Default) [TIER_0, TIER_1, TIER_2, TIER_3]
-  }))();
